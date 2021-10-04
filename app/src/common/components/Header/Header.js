d17c9e3c8ac49d5d02bb9ce2befe6799fb9e7913
keys <dandys10@live.com>
2021-10-03 23:00:36 -0500

    Add logo to header

--- app/src/common/components/Header/Header.js
+++ app/src/common/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 
 /**
  * Contains the shared header for each page. Only render user icon if logged in.
@@ -6,7 +7,7 @@ import { Box } from "@chakra-ui/layout";
  function Header(props) {
     return(
         <Box height="50px" bgColor="ce_darkgrey" width="100%">
-            {/** INSERT HEADER STUFF HERE */}
+            <Image src="/siu_logo.png" alt="SIU Logo" maxHeight="50px" />
         </Box>
     )
 }
