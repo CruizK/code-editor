77abb6add65e8029775e93f3b633fa2800be3f59
keys <dandys10@live.com>
2021-11-12 17:18:04 -0600

    Store markdown as actual markdown

--- app/src/modules/Tutorials/components/MarkdownEditor/MarkdownEditor.js
+++ app/src/modules/Tutorials/components/MarkdownEditor/MarkdownEditor.js
@@ -1,6 +1,7 @@
 import { useBreakpointValue } from '@chakra-ui/media-query';
 import { Input } from '@chakra-ui/react';
 import { convertToRaw, EditorState } from 'draft-js';
+import toMarkdown from 'draftjs-to-markdown';
 import dynamic from 'next/dynamic'; 
 import { useState } from 'react';
 const Editor = dynamic(
@@ -13,13 +14,13 @@ function MarkdownEditor() {
     const [editorState, setEditorState] = useState(
         () => EditorState.createEmpty(),
     );
-    const [raw, setRaw] = useState('');
+    const [markdown, setMarkdown] = useState('');
 
     const handleEditorChange = (state) => {
         setEditorState(state);
         let currentContent = convertToRaw(state.getCurrentContent());
-        let stringified = JSON.stringify(currentContent);
-        setRaw(stringified);
+        let newMarkdown = toMarkdown(currentContent);
+        setMarkdown(newMarkdown);
     };
 
     const maxWidth = useBreakpointValue({ base: "350px", lg: "768px"});
@@ -33,7 +34,7 @@ function MarkdownEditor() {
             editorClassName="demo-editor"
             wrapperStyle={{ maxWidth: maxWidth }}
         />
-        <Input id="md" type="hidden" value={raw} />
+        <Input id="md" type="hidden" value={markdown} />
         </>
     )
 }
