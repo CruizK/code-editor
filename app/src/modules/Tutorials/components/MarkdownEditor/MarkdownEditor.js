aabfc2397e002352870fda79a63f46af0b322093
keys <dandys10@live.com>
2021-11-12 16:56:43 -0600

    Store markdown into an input field

--- app/src/modules/Tutorials/components/MarkdownEditor/MarkdownEditor.js
+++ app/src/modules/Tutorials/components/MarkdownEditor/MarkdownEditor.js
@@ -1,4 +1,5 @@
 import { useBreakpointValue } from '@chakra-ui/media-query';
+import { Input } from '@chakra-ui/react';
 import { convertToRaw, EditorState } from 'draft-js';
 import dynamic from 'next/dynamic'; 
 import { useState } from 'react';
@@ -11,15 +12,20 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 function MarkdownEditor() {
     const [editorState, setEditorState] = useState(
         () => EditorState.createEmpty(),
-    )
+    );
+    const [raw, setRaw] = useState('');
 
     const handleEditorChange = (state) => {
         setEditorState(state);
+        let currentContent = convertToRaw(state.getCurrentContent());
+        let stringified = JSON.stringify(currentContent);
+        setRaw(stringified);
     };
 
     const maxWidth = useBreakpointValue({ base: "350px", lg: "768px"});
 
     return(
+        <>
         <Editor
             editorState={editorState}
             onEditorStateChange={handleEditorChange}
@@ -27,6 +33,8 @@ function MarkdownEditor() {
             editorClassName="demo-editor"
             wrapperStyle={{ maxWidth: maxWidth }}
         />
+        <Input id="md" type="hidden" value={raw} />
+        </>
     )
 }
 
