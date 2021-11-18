814fc102f4a6b0d7d2d266bcbd0ed0d48eecd1b3
BARTIMAEUS\embla <emblackwell312@gmail.com>
2021-11-17 18:41:19 -0600

    moved lastInProgressTutorial validation into command

--- api/CodeEditorApi/CodeEditorApi/Features/Tutorials/GetTutorials/GetUserLastInProgressTutorialCommand.cs
+++ api/CodeEditorApi/CodeEditorApi/Features/Tutorials/GetTutorials/GetUserLastInProgressTutorialCommand.cs
@@ -1,4 +1,6 @@
-﻿using CodeEditorApiDataAccess.Data;
+﻿using CodeEditorApi.Errors;
+using CodeEditorApi.Features.Courses.GetCourses;
+using CodeEditorApiDataAccess.Data;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -14,15 +16,40 @@ namespace CodeEditorApi.Features.Tutorials.GetTutorials
     public class GetUserLastInProgressTutorialCommand : IGetUserLastInProgressTutorialCommand
     {
         private readonly IGetTutorials _getTutorials;
+        private readonly IGetCourses _getCourses;
 
-        public GetUserLastInProgressTutorialCommand(IGetTutorials getTutorials)
+        public GetUserLastInProgressTutorialCommand(
+            IGetTutorials getTutorials,
+            IGetCourses getCourses)
         {
             _getTutorials = getTutorials;
+            _getCourses = getCourses;
         }
 
         public async Task<ActionResult<Tutorial>> ExecuteAsync(int userId, int courseId)
         {
-            return await _getTutorials.GetUserLastInProgressTutorial(userId, courseId);
+            var userRegisteredForCourse = await _getCourses.GetUserCourses(userId);
+            if (userRegisteredForCourse.Count() == 0)
+            {
+                return ApiError.BadRequest($"User is not registered for course with id {courseId}");
+            }
+
+            //this needs to grab tutorials based on both userId and courseId (tutorials under course)
+            var userRegisteredTutorials = await _getTutorials.GetUserRegisteredTutorials(userId);
+
+            if (!userRegisteredTutorials.Where(t => t.InProgress == true).Any())
+            {
+                return ApiError.BadRequest($"User has not started any tutorial for course with id {courseId}");
+            }
+
+            var tutorial = await _getTutorials.GetUserLastInProgressTutorial(userId, courseId);
+
+            if(tutorial == null)
+            {
+                return ApiError.BadRequest($"In Progress Tutorial could not be found.");
+            }
+
+            return tutorial;
         }
     }
 }
