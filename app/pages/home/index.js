6c47c03b624ae1d0b64ac5ae1d1b794f8d46cf59
keys <dandys10@live.com>
2021-11-21 02:25:08 -0600

    Get all published courses to pass into carousel

--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -1,12 +1,37 @@
 import Carousel from "@Components/Carousel/Carousel";
 import Main from "@Components/Main/Main";
+import { loggedIn } from "@Modules/Auth/Auth";
+import instance from "@Utils/instance";
 
-function Home() {
-  const carouselItems = [];
-  const randomInt = Math.max(Math.random() * 100, 2);
-  for (let index = 0; index < randomInt; index++) {
-    carouselItems.push(index);
+export async function getServerSideProps(context) {
+  var data = [];
+
+  const cookies = context.req.cookies;
+  const isLoggedIn = loggedIn(cookies.user);
+  const headers = {};
+
+  if (isLoggedIn) {
+    let token = cookies.user;
+    headers["Authorization"] = "Bearer " + token;
   }
+
+  let response = await instance.get("/Courses/GetAllPublishedCourses", {
+    headers: {...headers},
+  });
+
+  if (response.statusText == "OK")
+  data = response.data;
+
+  return {
+    props: {
+      courses: data,
+    }, // will be passed to the page component as props
+  }
+}  
+
+function Home(props) {
+  const { courses } = props;
+  const carouselItems = courses || []; // default to empty array
 
   return(
     <Main>
