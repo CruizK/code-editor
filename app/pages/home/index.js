b90a8d75b34e115c07714e9b304a9348dc6197bc
keys <dandys10@live.com>
2022-01-16 18:15:53 -0600

    Separate most popular courses call from the others
    The most popular courses carousel doesn't actually work just yet, but it's structured out.

--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -4,6 +4,7 @@ import Main from "@Components/Main/Main";
 import SectionHeader from "@Components/SectionHeader/SectionHeader";
 import SNoLink from "@Components/SNoLink/SNoLink";
 import { loggedIn } from "@Modules/Auth/Auth";
+import { getMostPopularCourses } from "@Modules/Courses/Courses";
 import instance from "@Utils/instance";
 
 export async function getServerSideProps(context) {
@@ -11,39 +12,28 @@ export async function getServerSideProps(context) {
 
   const cookies = context.req.cookies;
   const isLoggedIn = loggedIn(cookies.user);
-  const headers = {};
+  let token = cookies.user;
 
-  if (isLoggedIn) {
-    let token = cookies.user;
-    headers["Authorization"] = "Bearer " + token;
-  }
+  let popularCourses = await getMostPopularCourses(token);
 
-  // eventually, we'll need to handle all the course getters separately, but for now we don't have to care about the differentiation
-  // ideally we'd shove the request logic into modules/Home/Home.js
-
-  let response = await instance.get("/Courses/GetAllPublishedCourses", {
-    headers: {...headers},
-  });
-
-  if (response.statusText == "OK")
-  data = response.data;
+  if (popularCourses)
+  mostPopular = popularCourses;
 
   return {
     props: {
-      courses: data,
+      mostPopular: [], //mostPopular, TODO: Change once API route is fixed.
     }, // will be passed to the page component as props
   }
 }  
 
 function Home(props) {
-  const { courses } = props;
-  const carouselItems = courses || []; // default to empty array
+  const { mostPopular } = props;
 
   return(
     <Main>
       <Center><SNoLink href="/"><img src="/siucode_logo.png" /></SNoLink></Center>
-      <SectionHeader title="ALL COURSES" />
-      <Carousel items={carouselItems} />
+      <SectionHeader title="Most Popular" />
+      <Carousel items={mostPopular} />
     </Main>
   );
 }
